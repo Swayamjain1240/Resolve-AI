cd9b7ce22d6306e698dcc0d011a4592a71caa5a8
swayamjain1240 <swayamjain1240@gmail.com>
2026-09-02 22:12:20 +0530

    creating auth route and setup controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,27 @@
+import express from "express"
+
+
+export const Signup = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.error("error in Signup");
+        res.status(500).json({message:"internal server error"});
+    }
+}
+export const Login = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.error("error in Login");
+        res.status(500).json({message:"internal server error"});
+    }
+}
+export const Logout = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        console.error("error in Logout");
+        res.status(500).json({message:"internal server error"});
+    }
+}
